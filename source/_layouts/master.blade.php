f25082bc2ec2fd05de26aab99f74a4f4d98eba28
rick <rickwestdev@gmail.com>
2019-06-19 21:12:09 +0100

    wip

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
     </head>
     <body>
         @yield('body')
--- source/index.blade.php
+++ source/index.blade.php
@@ -2,4 +2,15 @@
 
 @section('body')
 <h1>Hello world!</h1>
+<script>
+    if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+            if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/admin/";
+                });
+            }
+        });
+    }
+</script>
 @endsection
